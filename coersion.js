1a6e936a4d1f42bb2d857ba6e83f0e99f3ddfb69
sakshi rathore <sakshi.17jics104@jietjodhpur.ac.in>
2021-03-17 23:49:16 +0530

    pdf of basic of Js by kyle

---
+++ coersion.js
@@ -0,0 +1,16 @@
+var num = 19;
+console.log(
+    `there are ${String(num)} student threre`
+);
+function add(num) {
+    return num + 1;
+}
+
+console.log(add(num++));
+var num="a"
+if(num)
+{
+    num = Number("a");
+}
+num = Number(9);
+console.log(num*2);
